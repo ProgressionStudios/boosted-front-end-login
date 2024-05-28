9f076a04208dd3294173df3a3c73ecc67d997012
Michael Garcia <michael@progressionstudios.com>
2024-05-27 22:57:28 -0700

    Update page activation code to be more consise

--- includes/page-activation.php
+++ includes/page-activation.php
@@ -10,82 +10,37 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+function boosted_check_if_page_exists($title, $content, $option_name) {
+    $query = new \WP_Query( array(
+        'post_type'      => 'page',
+        'post_status'    => 'publish',
+        'title'          => $title,
+        'posts_per_page' => 1,
+    ) );
+
+    if ( ! $query->have_posts() ) {
+        $page = array(
+            'post_title'    => $title,
+            'post_content'  => $content,
+            'post_status'   => 'publish',
+            'post_type'     => 'page',
+        );
+
+        $post_id = wp_insert_post( $page );
+
+        if ($post_id) {
+            update_option( $option_name, $post_id );
+        }
+    } else {
+        $page = $query->posts[0];
+        update_option( $option_name, $page->ID );
+    }
+}
+
 function create_login_pages() {
-    $login_query = new \WP_Query( array(
-        'post_type'      => 'page',
-        'post_status'    => 'publish',
-        'title'          => 'Login',
-        'posts_per_page' => 1,
-    ) );
-
-    if ( ! $login_query->have_posts() ) {
-        $login_page = array(
-            'post_title'    => 'Login',
-            'post_content'  => '<!-- wp:boosted/boosted-front-end-login /-->',
-            'post_status'   => 'publish',
-            'post_type'     => 'page',
-        );
-
-        $login_post_id = wp_insert_post( $login_page );
-
-        if ($login_post_id) {
-            update_option( 'boosted_login_page_id', $login_post_id );
-        }
-    } else {
-        $login_page = $login_query->posts[0];
-        update_option( 'boosted_login_page_id', $login_page->ID );
-    }
-
-    $lost_password_query = new \WP_Query( array(
-        'post_type'      => 'page',
-        'post_status'    => 'publish',
-        'title'          => 'Lost Password?',
-        'posts_per_page' => 1,
-    ) );
-
-    if ( ! $lost_password_query->have_posts() ) {
-        $lost_password_page = array(
-            'post_title'    => 'Lost Password?',
-            'post_content'  => '<!-- wp:boosted/boosted-lost-password /-->',
-            'post_status'   => 'publish',
-            'post_type'     => 'page',
-        );
-
-        $lost_password_post_id = wp_insert_post( $lost_password_page );
-
-        if ($lost_password_post_id) {
-            update_option( 'boosted_lost_password_page_id', $lost_password_post_id );
-        }
-    } else {
-        $lost_password_page = $lost_password_query->posts[0];
-        update_option( 'boosted_lost_password_page_id', $lost_password_page->ID );
-    }
-
-    $registration_query = new \WP_Query( array(
-        'post_type'      => 'page',
-        'post_status'    => 'publish',
-        'title'          => 'Register',
-        'posts_per_page' => 1,
-    ) );
-
-    if ( ! $registration_query->have_posts() ) {
-        $registration_page = array(
-            'post_title'    => 'Register',
-            'post_content'  => '<!-- wp:boosted/boosted-registration /-->',
-            'post_status'   => 'publish',
-            'post_type'     => 'page',
-        );
-
-        $registration_post_id = wp_insert_post( $registration_page );
-
-        if ($registration_post_id) {
-            update_option( 'boosted_registration_page_id', $registration_post_id );
-        }
-    } else {
-        $registration_page = $registration_query->posts[0];
-        update_option( 'boosted_registration_page_id', $registration_page->ID );
-    }
-    
+    boosted_check_if_page_exists('Login', '<!-- wp:boosted/boosted-front-end-login /-->', 'boosted_login_page_id');
+    boosted_check_if_page_exists('Lost Password?', '<!-- wp:boosted/boosted-lost-password /-->', 'boosted_lost_password_page_id');
+    boosted_check_if_page_exists('Register', '<!-- wp:boosted/boosted-registration /-->', 'boosted_registration_page_id');
 }
 
 function lostpassword_page_link($lostpassword_url) {
