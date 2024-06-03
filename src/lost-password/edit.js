be7e5451cf987468df1124dceb3e46524ab50add
Michael Garcia <michael@progressionstudios.com>
2024-06-03 12:59:51 -0700

    Add Lost Password form fields and TextControls in the Block Editor

--- src/lost-password/edit.js
+++ src/lost-password/edit.js
@@ -1,14 +1,75 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, InspectorControls, RichText } from '@wordpress/block-editor';
+import { PanelBody, TextControl, TextareaControl } from '@wordpress/components';
 import './editor.scss';
 
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }) {
+	const {
+		lostDescription,
+		usernameLabel,
+		usernamePlaceholder,
+		resetButtonLabel,
+	} = attributes;
+
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __(
-				'Lost Password? – hello from the editor!',
-				'boosted-front-end-login'
-			) }
-		</p>
+		<>
+			<InspectorControls>
+				<PanelBody title={__('Lost Password Settings', 'boosted-front-end-login')}>
+					<TextareaControl
+						label={__('Description', 'boosted-front-end-login')}
+						value={lostDescription}
+						onChange={(value) => setAttributes({ lostDescription: value })}
+					/>
+					<TextControl
+						label={__('Username Label', 'boosted-front-end-login')}
+						value={usernameLabel}
+						onChange={(value) => setAttributes({ usernameLabel: value })}
+					/>
+					<TextControl
+						label={__('Username Placeholder', 'boosted-front-end-login')}
+						value={usernamePlaceholder}
+						onChange={(value) => setAttributes({ usernamePlaceholder: value })}
+					/>
+					<TextControl
+						label={__('Reset Password Button', 'boosted-front-end-login')}
+						value={resetButtonLabel}
+						onChange={(value) => setAttributes({ resetButtonLabel: value })}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<div {...useBlockProps()}>
+				<form className="boosted-front-end-form boosted-front-end-lost-password" method="post" action="#">
+					<p className="boosted-front-end-form-lost-description">
+						<RichText
+							tagName="label"
+							htmlFor="lost_description"
+							value={lostDescription}
+							onChange={(value) => setAttributes({ lostDescription: value })}
+							placeholder={__('Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'boosted-front-end-login')}
+						/>
+					</p>
+					<p className="boosted-front-end-username">
+						<RichText
+							tagName="label"
+							htmlFor="user_login"
+							value={usernameLabel}
+							onChange={(value) => setAttributes({ usernameLabel: value })}
+							placeholder={__('Username or Email', 'boosted-front-end-login')}
+						/>
+						<input className="boosted-front-end-username" type="text" id="username" name="username" required placeholder={usernamePlaceholder} />
+					</p>
+					<p class="boosted-front-end-form-submit">
+						<input
+							type="submit"
+							className="boosted-front-end-submit"
+							value={resetButtonLabel}
+							aria-label={__('Request a password reset', 'boosted-front-end-login')}
+							disabled
+						/>
+					</p>
+				</form>
+			</div>
+		</>
 	);
 }
--- src/lost-password/render.php
+++ src/lost-password/render.php
@@ -38,16 +38,18 @@
         </div>
     <?php endif; ?>
 
-    <form class="boosted-front-end-form" method="post" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" name="Lost-Password-Form">
+    <form class="boosted-front-end-form boosted-front-end-lost-password" method="post" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" name="Lost-Password-Form">
         <?php wp_nonce_field( 'front_end_lost_password_action', 'front_end_lost_password_nonce' ); ?>
         <input type="hidden" name="action" value="front_end_lost_password">
-        <p><?php esc_html_e('Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'boosted-front-end-login'); ?></p>
-        <p>
-            <label for="user_login"><?php esc_html_e('Username or Email', 'boosted-front-end-login'); ?></label>
-            <input class="boosted-front-end-username" type="text" id="user_login" name="user_login" required>
+        <?php if ( ! empty( $attributes['lostDescription'] ) ) : ?>
+            <p class="boosted-front-end-form-lost-description"><?php echo wp_kses_post( $attributes['lostDescription'] ); ?></p>
+        <?php endif; ?>
+        <p class="boosted-front-end-form-username">
+            <?php if ( ! empty( $attributes['usernameLabel'] ) ) : ?><label for="user_login"><?php echo wp_kses_post( $attributes['usernameLabel'] ); ?></label><?php endif; ?>
+            <input class="boosted-front-end-username" type="text" id="user_login" name="user_login" placeholder="<?php echo esc_attr( $attributes['usernamePlaceholder'] ); ?>" required>
         </p>
-        <p>
-            <input class="boosted-front-end-submit" type="submit" value="<?php esc_html_e('Reset Password', 'boosted-front-end-login'); ?>" aria-label="<?php esc_html_e('Request a password reset', 'boosted-front-end-login'); ?>">
+        <p class="boosted-front-end-form-submit">
+            <input class="boosted-front-end-submit" type="submit" value="<?php echo esc_attr( $attributes['resetButtonLabel'] ); ?>" aria-label="<?php esc_html_e('Request a password reset', 'boosted-front-end-login'); ?>">
         </p>
     </form>
 </div>
